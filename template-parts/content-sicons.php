f343b8ac07c7f739e081ee45c1c8725851a456d0
Javier Rodriguez <1719453+javsrod@users.noreply.github.com>
2022-12-26 12:25:52 -0800

    Create content-sicons.php

---
+++ template-parts/content-sicons.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Social Links
+ */
+?>
+<nav role="navigation" aria-label="Social">
+<h3 class="srt">Social Media Profile Links</h3>
+
+<ul class="sicons">
+	<li>
+		<a href="https://www.facebook.com/movitajuicebar" class="icon" target="_blank">
+			<span class="fab fa-facebook"></span><span class="hidden_visually">Facebook</span>
+		</a>
+	</li>
+	<li>
+		<a href="https://www.instagram.com/movitajuicebar/" class="icon" target="_blank">
+			<span class="fab fa-instagram"></span><span class="hidden_visually">instagram</span>
+		</a>
+	</li>
+    <li>
+        <a href="https://www.tiktok.com/@movitajuicebar" class="icon" target="_blank">
+            <span class="fab fa-tiktok"></span><span class="hidden_visually">tiktok</span>
+        </a>
+    </li>
+	<li>
+		<a href="https://www.youtube.com/channel/UCCauSXoqjbq57COlT8fZf2A/" class="icon" target="_blank">
+			<span class="fab fa-youtube"></span><span class="hidden_visually">You Tube</span>
+		</a>
+	</li>
+	<li>
+		<a href="https://twitter.com/movitajuicebar" class="icon" target="_blank">
+			<span class="fab fa-twitter"></span><span class="hidden_visually">Twitter</span>
+		</a>
+	</li>
+</ul>
+
+</nav>
