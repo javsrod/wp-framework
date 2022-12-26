2ab569436ff1d4159e7a87a8d7f0f0cf2760e938
Javier Rodriguez <1719453+javsrod@users.noreply.github.com>
2022-12-26 12:23:17 -0800

    Create nav-head.php

---
+++ template-parts/nav-head.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The Head Tag Stuff
+ *
+ */
+?>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <?php wp_head(); ?>
+    
+    <!-- Font Awesome Kit: certiprint -->
+    <script data-search-pseudo-elements defer src="https://kit.fontawesome.com/e92d9d3897.js" crossorigin="anonymous"></script>
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
+    
+</head>
