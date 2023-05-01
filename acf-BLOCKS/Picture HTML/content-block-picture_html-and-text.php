d239ad584f4df5e5ac0e53e7933994acae575a7b
Javier Rodriguez <1719453+javsrod@users.noreply.github.com>
2023-04-30 22:05:40 -0700

    Picture & Text (Upgrading Media & Text Default Block)

---
+++ acf-BLOCKS/Picture HTML/content-block-picture_html-and-text.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Block: Picture HTML & Text
+ * Expanding the default wordpress block: Media & Text.
+ * 
+ * @package JJROD
+ */
+
+// Create class attribute allowing for custom "className" values.
+$className = '';
+if( !empty($block['className']) ) {
+    $className .= ' ' . $block['className'];
+}
+
+?>
+
+<div class="wp-block-media-text is-stacked-on-mobile">
+    <figure class="wp-block-media-text__media">
+        <?php if( get_field('pic_desktop') ): ?>
+
+            <?php 
+                $link = get_field('pic_link');
+                if( $link ): 
+                    $link_url = $link['url'];
+                    $link_title = $link['title'];
+                    $link_target = $link['target'];
+                    ?>
+                    <a href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>">
+            <?php endif; ?>
+
+            <picture class="<?php echo esc_attr($className); ?>">
+
+                <?php if( get_field('pic_mobile') ): ?>
+                    <source
+                        media="(max-width: 700px)"
+                        src="<?php echo wp_get_attachment_image_url( get_field( 'pic_mobile' ), 'full' ); ?>"
+                        srcset="<?php echo wp_get_attachment_image_srcset( get_field( 'pic_mobile' ), 'full' ); ?>"
+                        sizes="(max-width: 1000px) 100vw, 1000px"
+                    >
+                <?php endif; ?>
+
+                <source
+                  src="<?php echo wp_get_attachment_image_url( get_field( 'pic_desktop' ), 'full' ); ?>"
+                  srcset="<?php echo wp_get_attachment_image_srcset( get_field( 'pic_desktop' ), 'full' ); ?>"
+                  sizes="(max-width: 2560px) 100vw, 2560px"
+                >
+
+                <img src="<?php echo wp_get_attachment_image_url( get_field( 'pic_desktop' ), 'full' ); ?>" alt="<?php echo get_post_meta(get_field( 'pic_desktop' ) , '_wp_attachment_image_alt', true); ?>">
+
+            </picture>
+            
+            <?php if( get_field('pic_link') ): ?> </a> <?php endif; ?>
+
+        <?php endif; ?>     
+    </figure>
+
+    <div class="wp-block-media-text__content">
+
+        <InnerBlocks />
+        
+    </div><!-- wp-block-media-text__content -->
+
+</div><!-- wp-block-media-text -->
+
+
+
+   
