bc574e1698674935cfc895e27e5a385c8cb57fe0
Javier Rodriguez <1719453+javsrod@users.noreply.github.com>
2023-09-11 22:46:14 -0700

    Update template-full_width_empty.php

--- template-full_width_empty.php
+++ template-full_width_empty.php
@@ -16,7 +16,8 @@ get_header();
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-     <?php the_content( ); ?>
+    <h1 class="hidden_visually"><?php the_title( ); ?></h1>
+    <?php the_content( ); ?>
 
  <?php endwhile; else : ?>
  
