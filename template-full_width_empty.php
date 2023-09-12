a2910a530a2c53fa0afb9c2e45423399e8b38bd1
Javier Rodriguez <1719453+javsrod@users.noreply.github.com>
2023-09-11 21:00:13 -0700

    Create template-full_width_empty.php

---
+++ template-full_width_empty.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 
+ * Template Name: Full Width Empty
+ * 
+ * Template with no styling or page header template called.
+ *
+ *
+ * @author JJROD Framework
+ * @see https://github.com/javsrod/wp-framework
+ * @version 1.0
+ */
+
+get_header();
+?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+     <?php the_content( ); ?>
+
+ <?php endwhile; else : ?>
+ 
+   <p><?php _e( 'Sorry, no pages found.' ); ?></p>
+ 
+ <?php endif; ?>  
+
+<?php get_footer(); ?>
