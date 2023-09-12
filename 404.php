ee48cf19a2d3fb24c4208a5f86baae394496d4aa
Javier Rodriguez <1719453+javsrod@users.noreply.github.com>
2023-09-11 20:50:53 -0700

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *  The 404 Page Template
+ * 
+ * @author JJROD Framework
+ * @see https://github.com/javsrod/wp-framework
+ * @version 1.0
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="sph spv-feat">
+     <div class="gcw-s">
+               <h1 class="category_heading">Bummer :/</h1> 
+               <p>Seems like we cant find that item.</p>
+     </div><!-- gcw-s -->
+</div><!-- sph --> 
+
+<?php get_footer(); ?>
