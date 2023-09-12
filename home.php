326bf15a42b16ffe7a819068980e75ea58635f6a
Javier Rodriguez <1719453+javsrod@users.noreply.github.com>
2023-09-11 20:43:31 -0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The main blog page template
+ * 
+ * @author JJROD Framework
+ * @see https://github.com/javsrod/wp-framework
+ * @version 1.0
+ * 
+ */
+
+get_header(); ?>
+			
+<main class="blog-home">
+    <div class="blog-header taft-w btm-shadow-light">
+        <div class="gcw">
+            <h1 class="mb-0">News</h1> 
+        </div>
+    </div><!-- .grid-container --> 
+
+    <div class="sph spt spb-feat">
+        <div class="gcw">
+            
+        <!-- ***************************** -->
+        <!-- Blog Posts -->
+        <!-- ***************************** -->
+
+            <div class="blog-archive-grid">
+
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <?php get_template_part( 'template-parts/blog/blog', 'loop-archive' ); ?>
+                <?php endwhile; ?>  
+                
+            </div><!-- blog-archive-grid -->
+
+            <?php joints_page_navi(); ?>
+            <?php else : ?>                     
+                <?php get_template_part( 'template-parts/content', 'missing' ); ?>   
+            <?php endif; ?>
+        </div><!-- gcw -->
+    </div><!-- sph-edge -->
+
+</main><!-- main -->
+
+<?php get_footer(); ?>
