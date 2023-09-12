cd07556a4a2b9dacf5fa43c94c56ee21b73e7db9
Javier Rodriguez <1719453+javsrod@users.noreply.github.com>
2023-09-11 21:09:46 -0700

    Update search.php

--- search.php
+++ search.php
@@ -9,28 +9,29 @@
 
 get_header(); ?>
 
-<main id="primary" class="site-main">
+<main>
+    
+    <div class="spv sph">
+        <div class="gcw">
 
-  <div class="spv sph">
-      <div class="gcw">
-          <header class="page-header archive-header">
-              <div class="container">
-                  <h1 class="page-title"><?php
-                  /* translators: %s: search query. */
-                  printf( esc_html__( 'Search Results for: %s', 'tbcparent' ), '<span>' . get_search_query() . '</span>' );?></h1>
+            <header class="page-header archive-header">
+                <div class="container">
+                    <h1 class="page-title"><?php
+                    /* translators: %s: search query. */
+                    printf( esc_html__( 'Search Results for: %s', 'tbcparent' ), '<span>' . get_search_query() . '</span>' );?></h1>
 
-                  </div>
-          </header><!-- .page-header -->
+                </div>
+            </header><!-- .page-header -->
 
-          <!-- ***************************** -->
-          <!-- Posts -->
-          <!-- ***************************** -->
+            <!-- ***************************** -->
+            <!-- Posts -->
+            <!-- ***************************** -->
 
-          <div class="">
-              <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                  <?php get_template_part( 'template-parts/loop', 'archive' ); ?>
-              <?php endwhile; ?>  
-          </div>
+            <div class="">
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <?php get_template_part( 'template-parts/blog/bloog', 'loop-archive' ); ?>
+                <?php endwhile; ?>  
+            </div>
 
           <?php joints_page_navi(); ?>
 
@@ -38,14 +39,14 @@ get_header(); ?>
               <?php get_template_part( 'template-parts/content', 'missing' ); ?>   
           <?php endif; ?>
 
-     </div><!-- gcw -->
-  </div><!-- spv sph -->
+        </div><!-- gcw -->
+    </div><!-- spv sph -->
 
-  <div class="spv sph">
-      <div class="gcw">
-              <?php get_search_form(); ?>        
-      </div><!-- gcw -->
-  </div><!-- spv sph -->
+    <div class="spv sph">
+        <div class="gcw">
+                  <?php get_search_form(); ?>        
+        </div><!-- gcw -->
+    </div><!-- spv sph -->
 
 </main><!-- main -->
 
