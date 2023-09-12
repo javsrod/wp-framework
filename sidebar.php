9eb793f81227ab5eb3f62537b028f088184aa6f1
Javier Rodriguez <1719453+javsrod@users.noreply.github.com>
2023-09-11 21:12:19 -0700

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template the primary sidebar
+ *
+ * @author JJROD Framework
+ * @see https://github.com/javsrod/wp-framework
+ * @version 1.0
+ */
+ ?>
+
+<?php if( !dynamic_sidebar( 'primary' )) : ?>
+
+	<h2 class="module-heading">Sidebar setup</h2>
+	<p>Please add widgets!</p>
+
+<?php endif; ?>
