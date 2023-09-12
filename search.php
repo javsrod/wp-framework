d7cb8f5a2790134cb112640840dbfcad582c7690
Javier Rodriguez <1719453+javsrod@users.noreply.github.com>
2023-09-11 21:07:08 -0700

    Update search.php

--- search.php
+++ search.php
@@ -2,9 +2,9 @@
 /**
  * The template for displaying search results pages
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
- *
- * @package tbcparent
+ * @author JJROD Framework
+ * @see https://github.com/javsrod/wp-framework
+ * @version 1.0
  */
 
 get_header(); ?>
