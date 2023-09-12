9d5fd5bf73752c8337a0e550d353be45cd956d3e
Javier Rodriguez <1719453+javsrod@users.noreply.github.com>
2023-09-11 22:11:12 -0700

    Update home.php

--- home.php
+++ home.php
@@ -26,33 +26,34 @@ get_header(); ?>
     <!-- END Blog Header -->
     <!-- ***************************** -->
 
-    <!-- ***************************** -->
-    <!-- Blog Posts -->
-    <!-- ***************************** -->
-
     <div class="sph spt spb-feat">
         <div class="gcw">
             
-            <div class="blog-archive-grid">
+           <!-- ***************************** -->
+           <!-- Blog Posts -->
+           <!-- ***************************** -->
 
-                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                    <?php get_template_part( 'template-parts/blog/blog', 'archive' ); ?>
-                <?php endwhile; ?>  
-                
-            </div><!-- blog-archive-grid -->
+           <div class="blog-archive-grid">
 
-            <?php joints_page_navi(); ?>
+               <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                   <?php get_template_part( 'template-parts/blog/blog', 'archive' ); ?>
+               <?php endwhile; ?>  
+               
+           </div><!-- blog-archive-grid -->
 
-            <?php else : ?>                     
-                <?php get_template_part( 'template-parts/content', 'missing' ); ?>   
-            <?php endif; ?>
+           <?php joints_page_navi(); ?>
+
+           <?php else : ?>                     
+               <?php get_template_part( 'template-parts/content', 'missing' ); ?>   
+           <?php endif; ?>
+
+           <!-- ***************************** -->
+           <!-- END Blog Posts -->
+           <!-- ***************************** -->
+
 
         </div><!-- gcw -->
     </div><!-- sph-edge -->
-
-    <!-- ***************************** -->
-    <!-- END Blog Posts -->
-    <!-- ***************************** -->
 
 </main><!-- main -->
 
