5c048dc0c6112d9b023915c6f1aeede27e2d66c2
Javier Rodriguez <1719453+javsrod@users.noreply.github.com>
2023-09-19 16:47:26 -0700

    Update template-legal.php

--- template-legal.php
+++ template-legal.php
@@ -8,49 +8,56 @@
  *
  * @author JJROD Framework
  * @see https://github.com/javsrod/wp-framework
- * @version 1.0
+ * @version 1.1
  */
 
 get_header();
 ?>
 
-    
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <div class="gcw gss--3-1 grid-gap spv legal-page spb-feat">
-    
-        <div>
-            <h1><?php the_title( ); ?></h1>
-            <div class="legal-content spt">
-                <?php the_content( ); ?>
-            </div>
-        </div>
-
-        <aside>
-            <h3>Legal Menu</h3>
-            <nav>
-                <?php
-
-                $defaults = array(
-                'container' => false,
-                'theme_location' => 'legal-menu',
-                'menu_class' => 'no-bullet'
-                );
-
-                wp_nav_menu( $defaults );
-
-                ?>
-            </nav>
+    <div class="sph spt spb-feat">        
+        <div class="gcw gss--3-1 grid-gap legal-page ">
             
-        </aside>
+            <div class="spb">
+                <h1><?php the_title( ); ?></h1>
+                <div class="legal-content spt">
+                    <?php the_content( ); ?>
+                </div>
+            </div>
 
+            <aside class="sticky-parent">
 
-    </div> <!-- gcw #content -->
+                <div class="sticky">
+                    <h3>Legal Menu</h3>
+                    <nav>
+                        <?php
 
-    <?php endwhile; else : ?>
+                        $defaults = array(
+                        'container' => false,
+                        'theme_location' => 'legal-menu',
+                        'menu_class' => 'no-bullet'
+                        );
 
+                        wp_nav_menu( $defaults );
+
+                        ?>
+                    </nav>
+                </div><!-- sticky -->
+
+            </aside><!-- sticky-parent -->
+
+        </div><!-- gcw -->
+    </div><!--  sph spv  -->
+
+<?php endwhile; else : ?>
+
+    <div class="sph spv-feat"> 
         <p><?php _e( 'Sorry, no pages found.' ); ?></p>
+    </div><!--  sph spv-feat  -->
 
-    <?php endif; ?>
+
+<?php endif; ?>
 
 <?php get_footer(); ?>
+
