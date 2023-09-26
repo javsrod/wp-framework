d0efe4460ce2f94cdc8beb75303f8fe54e4c01cf
Javier Rodriguez <1719453+javsrod@users.noreply.github.com>
2023-09-25 23:35:45 -0700

    Create main.js

---
+++ assets/scripts/main.js
@@ -0,0 +1,13 @@
+//------------------------------------
+// OPTIONS
+//------------------------------------
+
+jQuery(document).ready(function($) {
+
+    // Canvas Slide Toggle
+    $( ".canvas_side_toggle" ).click(function() {
+        $("#canvas_slide_wrapper").toggleClass("open");
+        return false;
+    });
+
+}); // jQuery(document).ready(function($) END
