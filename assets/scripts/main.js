e4cbe8e6a2b3376622943ec18a7578795042356f
Javier Rodriguez <1719453+javsrod@users.noreply.github.com>
2023-09-25 23:36:29 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,13 +1,15 @@
-//------------------------------------
-// OPTIONS
-//------------------------------------
-
 jQuery(document).ready(function($) {
 
     // Canvas Slide Toggle
     $( ".canvas_side_toggle" ).click(function() {
         $("#canvas_slide_wrapper").toggleClass("open");
+        $(".main").toggleClass("opened-main");
+        $( ".opened-main" ).click(function() {
+            $("#canvas_slide_wrapper").removeClass("open");
+            $(".main").removeClass("opened-main");
+            return false;
+        });
+
         return false;
     });
 
-}); // jQuery(document).ready(function($) END
