aeec0ab48c2c6dc03b34417d474c19301367401d
Javier Rodriguez <1719453+javsrod@users.noreply.github.com>
2024-04-07 15:17:48 -0700

    Remove ACF HTML

--- inc/inc-acf.php
+++ inc/inc-acf.php
@@ -1,5 +1,20 @@
 <?php
 /**
+ *  Remove Inner Blocks ACF HTML - <div class="acf-inner-blocks-container"></div>
+ * 
+ * @see https://www.advancedcustomfields.com/resources/whats-new-with-acf-blocks-in-acf-6/#block-versioning
+ */
+
+add_filter( 'acf/blocks/wrap_frontend_innerblocks', 'acf_should_wrap_innerblocks', 10, 2 );
+function acf_should_wrap_innerblocks( $wrap, $name ) {
+    if ( $name == 'acf/test-block' ) {
+        return true;
+    }
+    return false;
+}
+
+
+/**
  *  Activate ACF Blocks Folder
  * 
  * @author JJROD Framework
